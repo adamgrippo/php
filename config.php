77dae6e3556eec0cffceb2753742cf8158a19fe9
"Taylor" <adamgrippo@iCloud.com>
2015-05-11 03:23:09 -0400

    Update config.php

--- config.php
+++ config.php
@@ -7,5 +7,9 @@
  * @const DB_PASSWORD
  * @const DB_NAME
  */
+defined( "DB_SERVER" )    ? null : define( "DB_SERVER", "" ) ;
+defined( "DB_USER" )      ? null : define( "DB_USER", "" ) ;
+defined( "DB_PASSWORD" )  ? null : define( "DB_PASSWORD", "" ) ;
+defined( "DB_NAME" )      ? null : define( "DB_NAME", "" ) ;
 
 ?>
