d0104e269681dfccdb952741147c7212cdac7d0b
Adam Grippo <adamgrippo@iCloud.com>
2021-08-07 01:52:56 -0400

    Update class-mysql-database.php

--- class-mysql-database.php
+++ class-mysql-database.php
@@ -34,12 +34,19 @@ class MySqlDatabase implements InterfaceDatabase {
 		return self::$instance ;
 	}
 	
+	/**
+	 * @const DB_SERVER
+	 * @const DB_USER
+	 * @const DB_PASSWORD
+	 * @const DB_NAME
+	 * @return void 
+	 */
 	public function db_open(  ) {
 		$this->connection = mysqli_connect( DB_SERVER, DB_USER, DB_PASSWORD, DB_NAME ) ;
-		if( mysqli_connect_errno(  ) ) :
+		if( $this->connection->connect_errno(  ) ) :
 			throw new \Exception( "Database connection failed: " .
-				mysqli_connect_error(  ) .
-				" ( " . mysqli_connect_errno(  ) . " ) "
+				$this->connection->connect_error(  ) .
+				" ( " . $this->connection->connect_errno(  ) . " ) "
 			) ;
 		endif ;
 	}
